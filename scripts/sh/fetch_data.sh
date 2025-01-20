f702275e56a0640b985596a22af5d364695cad62
Fernando Sanz-Lázaro <fsanzl@gmail.com>
2025-01-20 09:34:40 +0100

    tm -> facs

--- scripts/sh/fetch_data.sh
+++ scripts/sh/fetch_data.sh
@@ -5,7 +5,7 @@ rm -rf ./data/pre-process
 wget https://github.com/Hanslick-Online/hsl-transkribus-export/archive/refs/heads/main.zip
 unzip main
 mkdir ./data/pre-process
-mkdir ./data/tmp
+mkdir ./data/facs
 mv hsl-transkribus-export-main/tei/284592/*.xml ./data/pre-process
 mv hsl-transkribus-export-main/mets/284592/*.xml ./data/facs
 rm -rf hsl-transkribus-export-main
