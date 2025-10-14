f34de7643bd044c08eb4645a451ae64307dd7036
fsanzl <>
2025-10-14 11:27:34 +0200

    normalising names

--- scripts/py/renameFiles.py
+++ scripts/py/renameFiles.py
@@ -23,10 +23,11 @@ import re
 
 
 # list of files that should be ignored, such as system files like 'Thumbs.db'
-ignoreFiles = ['Thumbs.db', '.DS_Store']
+ignoreFiles = ["Thumbs.db", ".DS_Store"]
 
 # list of file formats in lower case that should be ignored, such as 'xlsx'
 ignoreFileExtensions = []
+
 
 def normaliseName(value):
     """
@@ -40,41 +41,41 @@ def normaliseName(value):
     # split into name and extension
     newValue, fileExt = os.path.splitext(value)
     # replace umlauts with two letters
-    newValue = newValue.replace('ä','ae')
-    newValue = newValue.replace('ö','oe')
-    newValue = newValue.replace('ü','ue')
-    newValue = newValue.replace('Ä','Ae')
-    newValue = newValue.replace('Ö','Oe')
-    newValue = newValue.replace('Ü','Ue')
-    newValue = newValue.replace('ß','ss')
+    newValue = newValue.replace("ä", "ae")
+    newValue = newValue.replace("ö", "oe")
+    newValue = newValue.replace("ü", "ue")
+    newValue = newValue.replace("Ä", "Ae")
+    newValue = newValue.replace("Ö", "Oe")
+    newValue = newValue.replace("Ü", "Ue")
+    newValue = newValue.replace("ß", "ss")
     # replace all other special characters
     # normalise, i. e. replace e.g. é with e
-    newValue = unicodedata.normalize('NFKD', newValue).encode('ascii', 'ignore')
-    newValue = newValue.decode('utf-8')
+    newValue = unicodedata.normalize("NFKD", newValue).encode("ascii", "ignore")
+    newValue = newValue.decode("utf-8")
     # some custom rules to serve as example
-#    newValue = newValue.replace(', ','_')
-#    newValue = newValue.replace(',','_')
-#    newValue = newValue.replace('+','_')
-#    newValue = newValue.replace(' - ','-')
-#    newValue = newValue.replace(' ','_')
+    #    newValue = newValue.replace(', ','_')
+    #    newValue = newValue.replace(',','_')
+    #    newValue = newValue.replace('+','_')
+    #    newValue = newValue.replace(' - ','-')
+    #    newValue = newValue.replace(' ','_')
     # you can also use regular expressions e. g.:
     # newValue = str(re.sub(r'(\()([\d]\))', r'-\2', newValue))
     # '( one number )' becomes '-number)'
 
     # all remaining invalid characters are removed
     # \ and / are kept to keep the path
-    newValue = str(re.sub('[^a-zA-Z0-9_\-/\\\]', '', newValue))
+    newValue = str(re.sub("[^a-zA-Z0-9_\-/\\\]", "", newValue))
 
-    return newValue+fileExt
+    return newValue + fileExt
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     # set path for xml files
-    path = u'./data/tmp'
+    path = "./data/tmp"
     # set rename to 'true' if renaming should be done
-    rename = 'true'
-    print ("######################################################")
-    print ("Working with directory: "+path+"\n")
+    rename = "true"
+    print("######################################################")
+    print("Working with directory: " + path + "\n")
 
     # scan full directory and create list with files and list with
     # directories, that serve as basis for further processing.
@@ -84,22 +85,22 @@ if __name__=="__main__":
 
     for root, dirs, files in os.walk(path):
         for dir in dirs:
-            dirList.append(os.path.join(root,dir).encode('utf-8'))
+            dirList.append(os.path.join(root, dir).encode("utf-8"))
         for file in files:
-            fileDirList.append(os.path.join(root,file).encode('utf-8'))
+            fileDirList.append(os.path.join(root, file).encode("utf-8"))
 
     # write file with list of files
-    outFile1 = open('filelist.csv','wb')
+    outFile1 = open("filelist.csv", "wb")
     for line in fileDirList:
-        writeLine = line.decode('utf-8')
-        outFile1.write((writeLine+'\n').encode('utf-8'))
+        writeLine = line.decode("utf-8")
+        outFile1.write((writeLine + "\n").encode("utf-8"))
     outFile1.close()
 
     # write file with list of directories
-    outFile2 = open('dirlist.csv','wb')
+    outFile2 = open("dirlist.csv", "wb")
     for line in dirList:
-        writeLine = line.decode('utf-8')
-        outFile2.write((writeLine+'\n').encode('utf-8'))
+        writeLine = line.decode("utf-8")
+        outFile2.write((writeLine + "\n").encode("utf-8"))
     outFile2.close()
 
     # rename files
@@ -111,41 +112,56 @@ if __name__=="__main__":
     renamedFiles = []
     for file in fileDirList:
         oldPath, origFileName = os.path.split(file)
-        fileCounter+= 1
+        fileCounter += 1
 
         # ignore system files according to list
-        if origFileName.decode('utf-8') in ignoreFiles:
-            print('ignoring: '+os.path.join(oldPath.decode('utf-8'),origFileName.decode('utf-8')))
+        if origFileName.decode("utf-8") in ignoreFiles:
+            print(
+                "ignoring: "
+                + os.path.join(oldPath.decode("utf-8"), origFileName.decode("utf-8"))
+            )
             continue
 
         # ignore files with extension according to list
         # can only be done when there is an extension
-        origFileNamePart, origFileExtension = os.path.splitext(origFileName.decode('utf-8'))
+        origFileNamePart, origFileExtension = os.path.splitext(
+            origFileName.decode("utf-8")
+        )
         if len(origFileExtension) == 0:
-            print('File does not have extension:',  origFileName.decode('utf-8'))
+            print("File does not have extension:", origFileName.decode("utf-8"))
         else:
-            if origFileName.decode('utf-8').rsplit('.', 1)[1].lower() in ignoreFileExtensions:
-                print('ignoring: '+os.path.join(oldPath.decode('utf-8'),origFileName.decode('utf-8')))
+            if (
+                origFileName.decode("utf-8").rsplit(".", 1)[1].lower()
+                in ignoreFileExtensions
+            ):
+                print(
+                    "ignoring: "
+                    + os.path.join(
+                        oldPath.decode("utf-8"), origFileName.decode("utf-8")
+                    )
+                )
                 continue
 
         # get normalised (renamed) file name
-        newFile = normaliseName(origFileName.decode('utf-8'))
-        newFilePath = os.path.join(oldPath.decode('utf-8'), newFile)
+        newFile = normaliseName(origFileName.decode("utf-8"))
+        newFilePath = os.path.join(oldPath.decode("utf-8"), f"v__{newFile}")
         # append old file with path and new file with path to list
         # new file path might be same as before, also append new file name
-        renamedFiles.append([file.decode('utf-8'), newFilePath, newFile])
+        renamedFiles.append([file.decode("utf-8"), newFilePath, newFile])
 
-        if newFile != origFileName.decode('utf-8'):
-            fileRenameCounter+=1
-            print('Normalised "', origFileName.decode('utf-8'), '" to ', newFile)
+        if newFile != origFileName.decode("utf-8"):
+            fileRenameCounter += 1
+            print('Normalised "', origFileName.decode("utf-8"), '" to ', newFile)
 
         # rename file if rename was set to true
         # collect files that cannot be renamed due to file name duplication
-        if rename == 'true':
+        if rename == "true":
             try:
                 os.rename(file, newFilePath)
             except FileExistsError:
-                print('The file could not be renamed, because a file with the same name already exists!')
+                print(
+                    "The file could not be renamed, because a file with the same name already exists!"
+                )
                 exceptedFiles.append(file)
 
     # rename directories, starting from within, i.e. reverse order of dirList
@@ -153,46 +169,58 @@ if __name__=="__main__":
     dirRenameCounter = 0
     renamedDirs = []
     for dir in dirList[::-1]:
-        dirCounter+= 1
+        dirCounter += 1
         oldPath, oldDir = os.path.split(dir)
-        newDir = normaliseName(oldDir.decode('utf-8'))
-        newDirPath = os.path.join(oldPath.decode('utf-8'), newDir)
+        newDir = normaliseName(oldDir.decode("utf-8"))
+        newDirPath = os.path.join(oldPath.decode("utf-8"), newDir)
         # append old directories with path and new directories with path to list
         # new path might be same as before, also append new directory name
-        renamedDirs.append([dir.decode('utf-8'), newDirPath, newDir])
+        renamedDirs.append([dir.decode("utf-8"), newDirPath, newDir])
 
-        if newDir != oldDir.decode('utf-8'):
-            dirRenameCounter+=1
-            print('Normalised "', oldDir.decode('utf-8'), '" to ', newDir)
+        if newDir != oldDir.decode("utf-8"):
+            dirRenameCounter += 1
+            print('Normalised "', oldDir.decode("utf-8"), '" to ', newDir)
 
-        if rename == 'true':
+        if rename == "true":
             os.rename(dir, newDirPath)
 
     actualFileRenameCounter = fileRenameCounter - len(exceptedFiles)
-    print('Renamed ', actualFileRenameCounter, ' files of a total of ', fileCounter, 'files.')
-    print('Renamed ', dirRenameCounter, ' directories of a total of ', dirCounter, 'directories.')
-    if len(exceptedFiles)<1:
-        print('No errors in renaming.')
+    print(
+        "Renamed ",
+        actualFileRenameCounter,
+        " files of a total of ",
+        fileCounter,
+        "files.",
+    )
+    print(
+        "Renamed ",
+        dirRenameCounter,
+        " directories of a total of ",
+        dirCounter,
+        "directories.",
+    )
+    if len(exceptedFiles) < 1:
+        print("No errors in renaming.")
     else:
-        print('Some files could not be renamed. Manual action is required.')
-        str_exceptedFiles = [x.decode('utf-8') for x in exceptedFiles]
-        print('\n'.join(str_exceptedFiles))
+        print("Some files could not be renamed. Manual action is required.")
+        str_exceptedFiles = [x.decode("utf-8") for x in exceptedFiles]
+        print("\n".join(str_exceptedFiles))
 
-    print('Creating file and directory list with new names')
+    print("Creating file and directory list with new names")
     # write file with list of files and new names
-    outFile3 = open('renamedFilelist.csv','wb')
-    outFile3.write(('Old file;New file;New file name'+'\n').encode('utf-8'))
+    outFile3 = open("renamedFilelist.csv", "wb")
+    outFile3.write(("Old file;New file;New file name" + "\n").encode("utf-8"))
     for entry in renamedFiles:
-        writeLine = ';'.join(entry) #entry.decode('utf-8')
-        outFile3.write((writeLine+'\n').encode('utf-8'))
+        writeLine = ";".join(entry)  # entry.decode('utf-8')
+        outFile3.write((writeLine + "\n").encode("utf-8"))
     outFile3.close()
 
     # write file with list of directories and new names
     # writing reverse order of renamedDirs
-    outFile4 = open('renamedDirlist.csv','wb')
-    outFile4.write(('Old path;New path;New folder name'+'\n').encode('utf-8'))
+    outFile4 = open("renamedDirlist.csv", "wb")
+    outFile4.write(("Old path;New path;New folder name" + "\n").encode("utf-8"))
     for entry in renamedDirs[::-1]:
-        writeLine = ';'.join(entry)
-        outFile4.write((writeLine+'\n').encode('utf-8'))
+        writeLine = ";".join(entry)
+        outFile4.write((writeLine + "\n").encode("utf-8"))
     outFile4.close()
-    print('Done')
+    print("Done")
