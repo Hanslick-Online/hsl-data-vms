df7af551e643bdbbc6d3d791a4ee10324407b123
fsanzl <>
2025-10-15 13:30:54 +0200

    fetch

---
+++ scripts/sh/fetch_data_arche.sh
@@ -0,0 +1,10 @@
+# bin/bash
+
+# fetch edition entities all
+rm -rf ./data/indices
+mkdir ./data/indices
+wget https://github.com/Hanslick-Online/hsl-entities/archive/refs/heads/main.zip
+unzip main
+mv hsl-entities-main/out/*.xml ./data/indices
+rm -rf hsl-entities-main
+# rm main.zip%
