2136fb4df079608eda4b921b2f86ba1818696ddc
fsanzl <>
2025-11-20 13:22:51 +0100

    renaming for arche

---
+++ scripts/sh/rename.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+for i in data/editions/*.xml ;do
+	filenam=`echo $i |cut -d / -f 3`
+	A=`echo ${filenam:0:4}`
+	B=`echo ${filenam:4:-1}`
+	mv ${i} data/editions/v__${A}_${B}
+done
