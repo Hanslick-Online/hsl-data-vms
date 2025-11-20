d977714e0921145fb3c80e6979810b240ad92c95
fsanzl <>
2025-11-20 13:26:02 +0100

    rename

--- scripts/sh/rename.sh
+++ scripts/sh/rename.sh
@@ -2,6 +2,6 @@
 for i in data/editions/*.xml ;do
 	filenam=`echo $i |cut -d / -f 3`
 	A=`echo ${filenam:0:4}`
-	B=`echo ${filenam:4:-1}`
+	B=`echo ${filenam:4}`
 	mv ${i} data/editions/v__${A}_${B}
 done
